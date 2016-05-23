386b7b91224cfecfb0d2ddb52ae95442dd2729a0
francois LE CORRE <lecorre.f@gmail.com>
2016-05-23 14:51:07 +0200

    queue_.rs modifications

--- src/krpsim/queue.rs
+++ src/krpsim/queue.rs
@@ -3,7 +3,7 @@ extern crate std;
 use super::livep::Livep;
 
 pub struct Queue<'a> {
-    lst: std::collections::BinaryHeap<&'a Livep<'a>>
+    lst: std::collections::BinaryHeap<Livep<'a>>
 }
 
 impl <'a> Queue <'a> {
@@ -13,17 +13,17 @@ impl <'a> Queue <'a> {
         }
     }
 
-    fn add(
+    pub fn add(
         &mut self,
-        process: &'a Livep<'a>,
+        process: Livep<'a>,
     ) -> () {
         self.lst.push(process);
     }
 
-    fn get_ended_process(
+    pub fn get_ended_process(
         &mut self,
         cycle: u64
-    ) -> Option<Vec<&Livep>> {
+    ) -> Option<Vec<Livep>> {
         if self.lst.peek().is_some() &&
            self.lst.peek().unwrap().cycle_end == cycle {
             let mut ret = Vec::new();
